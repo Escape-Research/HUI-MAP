e1f73a53a8c7efe5c64bc877b4c844ec3813345e
etl17 <etl17@escaperesearch.com>
2016-05-18 09:21:50 -0700

    Added sdPIC30F family datasheet

--- nbproject/Package-XC16_dsPIC30F6014A.bash
+++ nbproject/Package-XC16_dsPIC30F6014A.bash
@@ -10,9 +10,9 @@ CND_CONF=XC16_dsPIC30F6014A
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/hui-map.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=hui-map.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=hui-map.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/hui-map.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=hui-map.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=hui-map/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/hui-map.x/bin
+makeDirectory ${TMPDIR}/hui-map/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/hui-map.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/hui-map.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/hui-map.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/hui-map.tar *
 checkReturnCode
 
 # Cleanup
